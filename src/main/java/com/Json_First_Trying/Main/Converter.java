1e1d7e1460d428609151d6c7bb547640b2f8a04b
DmitryPoverov <did.ink@mail.ru>
2021-07-19 15:28:38 +0300

    First tryings on branch1

---
+++ src/main/java/com/Json_First_Trying/Main/Converter.java
@@ -0,0 +1,23 @@
+package com.Json_First_Trying.Main;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Converter {
+
+    private final static String baseFile = "user.json";
+
+    public static void toJSON(User user) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File(baseFile), user);
+        System.out.println("A json file created.");
+    }
+    public static User toJavaObject() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println("A mapper created.");
+        return mapper.readValue(new File(baseFile), User.class);
+    }
+}
+
