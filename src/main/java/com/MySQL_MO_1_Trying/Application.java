c889635da5e680394e2a50e99a3da79b98aab529
DmitryPoverov <did.ink@mail.ru>
2021-08-02 16:20:23 +0300

    My own first trying using of SQL in Java

--- src/main/java/com/MySQL_MO_1_Trying/Application.java
+++ src/main/java/com/MySQL_MO_1_Trying/Application.java
@@ -1,10 +1,8 @@
 package com.MySQL_MO_1_Trying;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application {
@@ -24,24 +22,50 @@ public class Application {
     private static final String SQL_INSERT_MEN =
             "INSERT INTO Person(name, surname, age) VALUES('Bill', 'Lack', 54), ('Jim', 'Gambol', 43), ('Sam', 'Smith', 31)";
 
-    public static void main(String[] args) {
+    private static final String SQL_SHOW_ALL =
+            "SELECT * FROM Person ";
 
+    public static void createDataBase(){
         try (Connection connect = MyConnect.getConnection();
              Statement state = connect.createStatement()) {
             System.out.println("Successful connection.");
             state.executeUpdate(SQL_CREATE_DATABASE);
             System.out.println("Successful creation of DB.");
+
+        } catch (SQLException | IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    public static void createTable(){
+        try (Connection connect = MyConnect.getConnection();
+             Statement state = connect.createStatement()) {
+            System.out.println("Successful connection.");
             state.executeUpdate(SQL_USE_DB);
             System.out.println("Successful start of using.");
             state.executeUpdate(SQL_CREATE_TABLE);
             System.out.println("Successful creation of TABLE.");
+
+        } catch (SQLException | IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    public static void addMen(){
+        try (Connection connect = MyConnect.getConnection();
+             Statement state = connect.createStatement()) {
+            System.out.println("Successful connection.");
+            state.executeUpdate(SQL_USE_DB);
+            System.out.println("Successful start of using.");
             state.executeUpdate(SQL_INSERT_MEN);
             System.out.println("successful appending of men");
 
         } catch (SQLException | IOException exc) {
             exc.printStackTrace();
         }
+    }
 
+    public static void addMan() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Set the name:");
         String name = sc.nextLine();
@@ -64,7 +88,92 @@ public class Application {
         } catch (SQLException | IOException exc) {
             exc.printStackTrace();
         }
+    }
 
+    public static void showAll() {
+        try (Connection connect = MyConnect.getConnection();
+             Statement state = connect.createStatement()) {
+            System.out.println("Successful connection.");
+            state.executeUpdate(SQL_USE_DB);
+            System.out.println("Successful start of using.");
+            ResultSet resultSet = state.executeQuery(SQL_SHOW_ALL);
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                String surName = resultSet.getString("surname");
+                int age = resultSet.getInt("age");
+
+                System.out.printf("| %3d | %10s | %10s | %d |\n", id, name, surName, age);
+            }
+        } catch (SQLException | IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    public static void showMenu() {
+        System.out.print("""
+                Enter:
+                  1 - to create a DB;
+                  2 - to create a table;
+                  3 - to add Men;
+                  4 - to add a man;
+                  0 - to exit.
+                  Your choice:\040""");
+    }
+
+    public static void actions() {
+
+        Scanner sc;
+        int userChoice = -1;
+        boolean working = true;
+
+        do {
+            sc = new Scanner(System.in);
+
+            try {
+                userChoice = sc.nextInt();
+            }
+            catch (InputMismatchException iME) {
+                System.out.print("Enter only numbers. ");
+            }
+
+            switch (userChoice) {
+                case 1 -> {
+                    createDataBase();
+                    showMenu();
+                }
+                case 2 -> {
+                    createTable();
+                    showMenu();
+                }
+                case 3 -> {
+                    addMen();
+                    showMenu();
+                }
+                case 4 -> {
+                    addMan();
+                    showMenu();
+                }
+                case 5 -> {
+                    showAll();
+                    showMenu();
+                }
+                default -> {
+                    System.out.println("You new choice:");
+                }
+                case 0 -> {
+                    working = false;
+                    System.out.println("EXIT...");
+                }
+            }
+        }
+        while (working);
+    }
+
+    public static void main(String[] args) {
+
+        showMenu();
+        actions();
 
     }
 }
